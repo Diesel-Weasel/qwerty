8ea624cf6e0bc73c54724678fba8e9532bd284ae
Diesel-Weasel <112838985+Diesel-Weasel@users.noreply.github.com>
2022-10-14 13:27:42 +0900

    Калькулятор BMI

--- Form2.cs
+++ Form2.cs
@@ -10,17 +10,17 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-            Form2 ff2 = new Form2();
-            ff2.Show();
+            Form3 ff3 = new Form3();
+            ff3.Show();
             this.Hide();
         }
     }
